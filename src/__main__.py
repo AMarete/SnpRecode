3dd1161cc2da332c61f58bc338b2dd455f92b0b5
Andrew Marete <54328490+AMarete@users.noreply.github.com>
2020-10-02 22:56:30 -0400

    Error catch bug fixed

--- src/__main__.py
+++ src/__main__.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from os.path import isdir, isfile
 from check_modules import py_version, pkg_requirements
 from funtools import welcome_message, bomb
 
@@ -19,7 +20,6 @@ def main():
     pkg_requirements()
     py_version()
     welcome_message()
-    args0 = ['-h', '--help']
     args1 = ['-F', '-O']
     args2 = ['-g', '-s', '-o', '-n', '-t', '-a']
 
@@ -40,6 +40,11 @@ def main():
         if set(args1) - gg:
             bomb(f'Missing argument {list(set(args1) - gg)}\n'
                  'Error: run `./snprecode -h` for complete arguments list\n')
+        if sys.argv[1] == "-F" or sys.argv[1] == "--FILEPATH":
+            if not isdir(sys.argv[1]):
+                bomb(f"Check if directory path (-F, --FILEPATH) exists and non-empty files in dir path\n"
+                     f"       Don't forget -O prefix or --OUT prefix\n")
+
         from garbage import check_dups
         if not check_dups():
             print("File check complete...\n")
@@ -55,6 +60,11 @@ def main():
         if set(args2) - gg:
             bomb(f"Missing argument {list(set(args2) - gg)}\n"
                  "Error: run `./snprecode -h` for complete arguments list\n")
+
+        #if [file_ for file_ in sys.argv[1:] if not isfile(file_) and '-o' not in sys.argv[1:]]:
+        if [file_ for file_ in sys.argv[1:] if not isfile(file_) and all(x not in sys.argv[1:] for x in ['-o', '--out'])]:
+            bomb(f"Check if all input files exists in file path and are non-empty\n")
+
         import fi2geno
         fi2geno
         del fi2geno
