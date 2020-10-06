ef7d01bd633a711a2a03b9e10f0aebd511382116
Andrew Marete <54328490+AMarete@users.noreply.github.com>
2020-10-06 13:17:07 -0400

    check if path exists and if files exist in path

---
+++ src/check_path.py
@@ -0,0 +1,15 @@
+from parse_args import my_parser
+from os.path import abspath, isdir
+from funtools import bomb, file_by_size, to_mat
+
+vcf_list = [file_ for file_ in file_by_size(my_parser().filePath, ['vcf']) if file_.endswith(('vcf', 'vcf.gz'))]
+plink_list = to_mat(sorted(file_by_size(my_parser().filePath, ["ped", "map"])), 2)
+file_list = vcf_list + plink_list
+
+
+# get a list of input files
+def check_path():
+    if not isdir(my_parser().filePath):
+        return bomb(f'No files found at path = {my_parser().filePath}\n')
+    elif isdir(my_parser().filePath) and not [item for item in  file_list if item.endswith(('vcf', 'vcf.gz', 'ped', 'ped'))]:
+        return bomb(f'No vcf or plink files found at path = {abspath(my_parser().filePath)}\n')
